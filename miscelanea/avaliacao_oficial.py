b37932ec086a521e2f4640c6b968e52e9cda6f91
julia godoy <leuzegodoyjulia@gmail.com>
2023-11-04 12:49:29 -0300

    add temario_data-wiki_21024_results

--- miscelanea/avaliacao_oficial.py
+++ miscelanea/avaliacao_oficial.py
@@ -7,55 +7,92 @@ import gc
 
 t_ini_proc = datetime.now()
 
+datasets = [
+# 'arthurmluz/temario_data-wiki_results',
+# 'arthurmluz/temario_data-wiki_1024_results',
+'arthurmluz/temario_data-wiki_temario_results',
+'arthurmluz/temario_data-temario_results',
+'arthurmluz/temario_data-xlsumm_results',
+'arthurmluz/temario_data-xlsumm_cstnews_results',
+'arthurmluz/temario_data-wikilingua_cstnews_results',
+'arthurmluz/temario_data-wikilingua_cstnews_1024_results',
+'arthurmluz/temario_data-xlsumm_cstnews_1024_results',
+'arthurmluz/temario_data-cstnews_results',
 
-nome_dataset = 'arthurmluz/temario_data-wiki_results'
-dataset = load_dataset(nome_dataset)['validation']
+'arthurmluz/GPTextSum_data-wiki_results',
+'arthurmluz/GPTextSum_data-wiki_1024_results',
+'arthurmluz/GPTextSum_data-wiki_cstnews_results',
+'arthurmluz/GPTextSum_data-wiki_cstnews_1024_results',
+'arthurmluz/GPTextSum_data-wiki_temario_results',
+'arthurmluz/GPTextSum_data-temario_results',
+'arthurmluz/GPTextSum_data-xlsumm_results',
+'arthurmluz/GPTextSum_data-xlsumm_cstnews_1024_results',
+'arthurmluz/GPTextSum_data-xlsumm_cstnews_results',
+'arthurmluz/GPTextSum_data-cstnews_results',
 
-print('total rows', dataset.num_rows)
+'arthurmluz/cstnews_data-wiki_results',
+'arthurmluz/cstnews_data-wiki_1024_results',
+'arthurmluz/cstnews_data-wiki_cstnews_results',
+'arthurmluz/cstnews_data-wiki_cstnews_1024_results',
+'arthurmluz/cstnews_data-temario_results',
+'arthurmluz/cstnews_data-wiki_temario_results',
+'arthurmluz/cstnews_data-xlsumm_results',
+'arthurmluz/cstnews_data-xlsumm_cstnews_results',
+'arthurmluz/cstnews_data-xlsumm_cstnews_1024_results',
+'arthurmluz/cstnews_data-cstnews_results',
 
-output_dir= f"resultados/{nome_dataset.split('/')[1].split('-')[0]}"
-nome = f"{output_dir}/{nome_dataset.split('/')[1]}"
-
-print(f'EXECUTANDO {nome}')
-
-# criar arquivo de output 
-Path(output_dir).mkdir(parents=True, exist_ok=True)
-with open(f'{nome}_fieldade.json', 'w', encoding='utf-8') as jsonFile:
-    json.dump([], jsonFile, indent=4)
-with open(f'{nome}_conteudo.json', 'w', encoding='utf-8') as jsonFile:
-    json.dump([], jsonFile, indent=4)
-
-
-for idx_row in range(dataset.num_rows):
-
-    id = dataset['id'][idx_row]
-    texto_original = dataset['text'][idx_row]
-    resumo_original = dataset['summary'][idx_row]
-    resumo_gerado = dataset['gen_summary'][idx_row]
-
-    t_ini = datetime.now()
-    av = Avaliacao(nome=nome, id=id, resumo_original=resumo_original, resumo_gerado=resumo_gerado, texto_original=texto_original)
-    av.avaliacao_conteudo()
-    
-    # pq memoria
-    del av
-    gc.collect()
-
-    av = Avaliacao(nome=nome, id=id, resumo_original=resumo_original, resumo_gerado=resumo_gerado, texto_original=texto_original)
-    av.avaliacao_fieldade()
-    t_fim = datetime.now()
-
-    del av
-    gc.collect()
-
-    print(f'({id}) concluido em {(t_fim-t_ini).seconds} segundos')
+'arthurmluz/wikilingua_data-wiki_results',
+'arthurmluz/wikilingua_data-wiki_1024_results',
+'arthurmluz/wikilingua_data-wiki_cstnews_results',
+'arthurmluz/wikilingua_data-wiki_cstnews_1024_results',
+'arthurmluz/wikilingua_data-wiki_temario_results',
+]
 
 
 
-t_fim_proc = datetime.now()
-print(f'concluído ({(t_fim_proc-t_ini_proc).seconds/60} min)')
+for nome_dataset in datasets:
+    dataset = load_dataset(nome_dataset)['validation']
+
+    print('total rows', dataset.num_rows)
+
+    output_dir= f"resultados/{nome_dataset.split('/')[1].split('-')[0]}"
+    nome = f"{output_dir}/{nome_dataset.split('/')[1]}"
+
+    print(f'EXECUTANDO {nome}')
+
+    # criar arquivo de output 
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    with open(f'{nome}_fieldade.json', 'w', encoding='utf-8') as jsonFile:
+        json.dump([], jsonFile, indent=4)
+    with open(f'{nome}_conteudo.json', 'w', encoding='utf-8') as jsonFile:
+        json.dump([], jsonFile, indent=4)
+
+
+    for idx_row in range(dataset.num_rows):
+
+        id = dataset['id'][idx_row]
+        texto_original = dataset['text'][idx_row]
+        resumo_original = dataset['summary'][idx_row]
+        resumo_gerado = dataset['gen_summary'][idx_row]
+
+        t_ini = datetime.now()
+        av = Avaliacao(nome=nome, id=id, resumo_original=resumo_original, resumo_gerado=resumo_gerado, texto_original=texto_original)
+        av.avaliacao_conteudo()
+
+        # pq memoria
+        del av
+        gc.collect()
+
+        av = Avaliacao(nome=nome, id=id, resumo_original=resumo_original, resumo_gerado=resumo_gerado, texto_original=texto_original)
+        av.avaliacao_fieldade()
+        t_fim = datetime.now()
+
+        del av
+        gc.collect()
+
+        print(f'({id}) concluido em {(t_fim-t_ini).seconds} segundos')
 
 
 
-
-
+    t_fim_proc = datetime.now()
+    print(f'concluído ({(t_fim_proc-t_ini_proc).seconds/60} min)')
