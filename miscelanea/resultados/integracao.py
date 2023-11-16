04a9bbad58d8284c6a64725a17fda9ccd94afa20
julia godoy <leuzegodoyjulia@gmail.com>
2023-11-15 22:09:51 -0300

    updates

--- miscelanea/resultados/integracao.py
+++ miscelanea/resultados/integracao.py
@@ -4,7 +4,7 @@ import math
 import numpy as np
 
 # removendo NaN
-diretorios = ['cstnews_data','GPTextSum_data','temario_data','wikilingua_data','xlsum_data']
+diretorios = ['cstnews_data','GPTextSum2_data','temario_data','wikilingua_data','xlsum_data']
 for diretorio in diretorios:
     for root, dirs, files in os.walk(diretorio):
         for file in files:
@@ -16,6 +16,8 @@ for diretorio in diretorios:
                 with open(filepath, 'w', encoding='utf-8') as f:
                     f.write(text.replace(': NaN', ': 0'))
                     print('%s updated' % filepath)
+
+
 
 # fonte: https://qa.fastforwardlabs.com/no%20answer/null%20threshold/bert/distilbert/exact%20match/f1/robust%20predictions/2020/06/09/Evaluating_BERT_on_SQuAD.html#:~:text=F1%20score%20is%20a%20common%20metric,of%20words%20in%20the%20ground%20truth.&text=F1%20score%20is%20a,in%20the%20ground%20truth.&text=is%20a%20common%20metric,of%20words%20in%20the
 # these functions are heavily influenced by the HF squad_metrics.py script
@@ -64,7 +66,6 @@ def compute_f1(prediction, truth):
     
     return 2 * (prec * rec) / (prec + rec)
 
-diretorios = ['cstnews_data','GPTextSum_data','temario_data']
 file_ok = []
 for diretorio in diretorios:
     for root, dirs, files in os.walk(diretorio):
--- miscelanea/resultados/relatorio.py
+++ miscelanea/resultados/relatorio.py
@@ -3,18 +3,24 @@ import json
 import numpy as np
 import pandas as pd
 
-df = pd.DataFrame(columns=['modelo', 'dataset','quality_cosseno','quality_bertscore','quality_moverscore','quality_rouge1','quality_rouge2','quality_rougel','quality_f1','quality_em',
-                                               'faithfulness_cosseno','faithfulness_bert','faithfulness_mover','faithfulness_rouge1','faithfulness_rouge2','faithfulness_rougel','faithfulness_f1','faithfulness_em'])
-diretorios = ['cstnews_data','GPTextSum_data','temario_data']
+df = pd.DataFrame(columns=[ 'dataset','modelo',
+                                               'quality_rouge1','quality_rouge2','quality_rougel','quality_f1','quality_em',
+                                               'quality_cosseno','quality_bertscore','quality_moverscore',
+                                               'faithfulness_rouge1','faithfulness_rouge2','faithfulness_rougel','faithfulness_f1','faithfulness_em',
+                                               'faithfulness_cosseno','faithfulness_bert','faithfulness_moverscore'])
+diretorios = ['cstnews_data','GPTextSum2_data','temario_data','wikilingua_data','xlsum_data']
 for diretorio in diretorios:
     for root, dirs, files in os.walk(diretorio):
         for file in files:
             if file.endswith('_results.json'):
                 config = file.replace('_results.json', '')\
                               .replace('xlsumm','XLSum')\
+                              .replace('xlsum','XLSum')\
                               .replace('wiki', 'Wiki')\
                               .replace('cstnews', 'CSTNews')\
                               .replace('temario', 'TeMario')\
+                              .replace('gptextsum2', 'GPTextSum')\
+                              .replace('GPTextSum2', 'GPTextSum')\
                               .split('_data-')
                 modelo, dataset = config[1].replace('_',' '), config[0]
 
@@ -52,20 +58,24 @@ for diretorio in diretorios:
                     faithfulness_f1.append(item['fieldade_f1'])
                     faithfulness_em.append(item['fieldade_em'])
 
-                quality_cosseno = round(np.average(quality_cosseno), 5)
-                quality_bertscore = round(np.average(quality_bertscore), 5)
-                quality_moverscore = round(np.average(quality_moverscore), 5)
-                quality_rouge1,quality_rouge2,quality_rougel = round(np.average(quality_rouge1), 5), round(np.average(quality_rouge2), 5), round(np.average(quality_rougel), 5)
-                quality_f1,quality_em = round(np.average(quality_f1), 5), round(np.average(quality_em), 5)
+                quality_cosseno = round(np.average(quality_cosseno), 4)
+                quality_bertscore = round(np.average(quality_bertscore), 4)
+                quality_moverscore = round(np.average(quality_moverscore), 4)
+                quality_rouge1,quality_rouge2,quality_rougel = round(np.average(quality_rouge1), 4), round(np.average(quality_rouge2), 4), round(np.average(quality_rougel), 4)
+                quality_f1,quality_em = round(np.average(quality_f1), 4), round(np.average(quality_em), 4)
 
-                faithfulness_cosseno = round(np.average(faithfulness_cosseno), 5)
-                faithfulness_bertscore = round(np.average(faithfulness_bertscore), 5)
-                faithfulness_moverscore = round(np.average(faithfulness_moverscore), 5)
-                faithfulness_rouge1,faithfulness_rouge2,faithfulness_rougel = round(np.average(faithfulness_rouge1), 5), round(np.average(faithfulness_rouge2), 5), round(np.average(faithfulness_rougel))
-                faithfulness_f1,faithfulness_em = round(np.average(faithfulness_f1), 5), round(np.average(faithfulness_em), 5)
+                faithfulness_cosseno = round(np.average(faithfulness_cosseno), 4)
+                faithfulness_bertscore = round(np.average(faithfulness_bertscore), 4)
+                faithfulness_moverscore = round(np.average(faithfulness_moverscore), 4)
+                faithfulness_rouge1,faithfulness_rouge2,faithfulness_rougel = round(np.average(faithfulness_rouge1), 4), round(np.average(faithfulness_rouge2), 4), round(np.average(faithfulness_rougel))
+                faithfulness_f1,faithfulness_em = round(np.average(faithfulness_f1), 4), round(np.average(faithfulness_em), 4)
 
-                df.loc[len(df)] = [modelo,dataset,quality_cosseno,quality_bertscore,quality_moverscore,quality_rouge1,quality_rouge2,quality_rougel,quality_f1,quality_em,
-                                    faithfulness_cosseno,faithfulness_bertscore,faithfulness_moverscore,faithfulness_rouge1,faithfulness_rouge2,faithfulness_rougel,faithfulness_f1,faithfulness_em]
+                df.loc[len(df)] = [dataset,modelo,
+                                   quality_rouge1,quality_rouge2,quality_rougel,quality_f1,quality_em,
+                                   quality_cosseno,quality_bertscore,quality_moverscore,
+                                   faithfulness_rouge1,faithfulness_rouge2,faithfulness_rougel,faithfulness_f1,faithfulness_em,
+                                   faithfulness_cosseno,faithfulness_bertscore,faithfulness_moverscore,
+                                ]
                 
-df.sort_values(['modelo','dataset'], inplace=True)
+df.sort_values(['dataset','modelo'], inplace=True)
 df.to_csv('relatorio.csv', index=False, encoding='utf-8')
